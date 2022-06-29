433326de4db6a24eca607617530fd36e0f4a1674
khw7876 <skanahrwi@naver.com>
2022-06-29 09:50:33 +0900

    feature/user user모델

---
+++ Friends_Back/user/serializers.py
@@ -0,0 +1,30 @@
+from user.models import User as UserModel
+from rest_framework import serializers
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+
+        return data
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+
+        user = UserModel(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+    def update(self, instance, validated_data):
+
+
+        return instance
+
+
+    class Meta:
+        model = UserModel
+        fields = ["username", "password", "username"]
--- Friends_Back/user/views.py
+++ Friends_Back/user/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from user.models import User as UserModel
+from user.serializers import UserSerializer
+
+
+class UserView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request):
+        all_users = UserModel.objects.all()
+        return Response(UserSerializer(all_users, many=True).data, status= status.HTTP_200_OK)
+        
+
+    def post(self,request):
+        user_serializer = UserSerializer(data=request.data, context={"request":request})
+    
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, obj_id):
+        user = UserModel.objects.get(id=obj_id)
+        user_serializer = UserSerializer(user, data=request.data, partial=True, context={"request":request})
+
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request):
+        return Response({"message" : "delete method!!"})
