7a68015109791e872638d104d112310f3946720a
최희원 <choehuiwon@choehuiwon-ui-MacBookAir.local>
2022-07-05 16:40:25 +0900

    edit/bookmark_test폴더 삭제

--- Friends_Back/bookmark_test/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from user.models import User as usermodel
-from joo_test.models import Post as postmodel
-# Create your models here.
--- Friends_Back/bookmark_test/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from bookmark_test import views
-urlpatterns = [
-    path('', views.BookmarktView.as_view()),
-    path('<int:post_id>', views.BookmarktView.as_view()),
-]
