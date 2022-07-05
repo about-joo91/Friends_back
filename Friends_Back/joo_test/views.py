f209445225b520dd6880de6a903ea73a00cbb63c
hajin <f-a-k@naver.com>
2022-07-06 00:34:59 +0900

    feat/ preview 테스트 완료/

--- Friends_Back/joo_test/views.py
+++ Friends_Back/joo_test/views.py
@@ -1,4 +1,5 @@
 import datetime
+import os 
 from urllib import parse
 
 import boto3
@@ -17,46 +18,79 @@ from config import AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY
 # Create your views here.
 
 
-class PostView(APIView):
-    
+class PreviewView(APIView):
+    def post(self,request):
+        
+        choice_char = request.data['choice']
+
+        s3= boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+
+        input_file = request.FILES['postimg']
+        input_name = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+        s3.put_object(
+                ACL= "public-read",
+                Bucket = "bucketfriends",
+                Body =input_file,
+                Key = input_name,
+                ContentType = input_file.content_type,
+            )
+        #### 테스트 코드 입니다. #####
+
+        # input_file = request.FILES['postimg']
+        # input_name = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        # event_name = str(request.user.id) + "preview"
+        # event_file = request.FILES['eventimg']
+        # file_list = []
+        # file_list.append(input_file)
+        # file_list.append(event_file)
+        # name_list= []
+        # name_list.append(input_name)
+        # name_list.append(event_name)
+        # for i in range(2):
+        #     file = file_list[i]
+        #     name = name_list[i]
+        #     print(file,name)
+        #     s3.put_object(
+        #         ACL= "public-read",
+        #         Bucket = "bucketfriends",
+        #         Body =file,
+        #         Key = name,
+        #         ContentType = file.content_type,
+        #     )
+        #### 딥러닝 출력이랑 로직 합쳐야 작동 합니다 ######
+        #### 합친 후 리턴 값 바꿔줘야 합니다 ####
+        return Response(input_name,status=status.HTTP_200_OK)
+
+
+
+class PostView(APIView):    
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
-
     def post(self, request):
-        title = request.POST.get('title')
-        content = request.POST.get('content')
         try:
-            s3= boto3.client(
-            's3',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            )
-            file = request.FILES['postimg']
-            date = (datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
-            s3.put_object(
-                    ACL= "public-read",
-                    Bucket = "bucketfriends",
-                    Body =file,
-                    Key = date,
-                    ContentType = file.content_type,
-                )
+            title = request.POST.get('title')
+            content = request.POST.get('content')
+            postimg = request.POST.get('postimg')
             author_id = request.user.id
-            base_url = "https://bucketfriends.s3.ap-northeast-2.amazonaws.com"
-            image_url = parse.urljoin(base=base_url,url=str(date),allow_fragments=True)
+            base_url = "https://bucketfriends.s3.ap-northeast-2.amazonaws.com/" + postimg
             post_datas = {
                 "author_id": author_id,
                 "title": title,
                 "content": content,
-                "postimg" : image_url,
+                "postimg" : base_url,
             }
             post_serializer = PostSerializer(data=post_datas)
             post_serializer.is_valid(raise_exception=True)
             post_serializer.save()
-
-            return Response(date,status=status.HTTP_200_OK)
-
+            return Response(status=status.HTTP_200_OK)
         except:
-            return Response({"message" : "게시글 업로드 실패."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
     def get(self, request):
         page_num = int(self.request.query_params.get('page_num'))
