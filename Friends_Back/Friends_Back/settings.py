29bbac442cd8f85d9fbaa0de82529c16561fc21c
khw7876 <skanahrwi@naver.com>
2022-07-05 20:35:44 +0900

    feat/ bookmark 일부 수정

--- Friends_Back/Friends_Back/settings.py
+++ Friends_Back/Friends_Back/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
 }
 REST_USE_JWT = True
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
--- Friends_Back/bookmark_test/views.py
+++ Friends_Back/bookmark_test/views.py
@@ -28,12 +28,13 @@ class BookmarktView(APIView):
     def post(self, request, post_id):
         save_user = request.user.id
         save_post = PostModel.objects.get(id=post_id)
-        same_save = SavePostModel.objects.filter(Q(save_user=save_user) & Q(save_post=save_post)).exists()
-        if not same_save:
+        same_save = SavePostModel.objects.filter(Q(save_user=save_user) & Q(save_post=save_post))
+        if not same_save.exists():
             bookmakrserializer = BookmarkSerializer(data=request.data)
             if bookmakrserializer.is_valid(raise_exception=True):
                 bookmakrserializer.save(save_user=request.user ,save_post=save_post)
                 return Response({"messge":"북마크가 완료되었습니다!"},status=status.HTTP_200_OK)
             return Response({"messge":"데이터 검증에 실패"},status=status.HTTP_400_BAD_REQUEST)
+        
         same_save.delete()
         return Response ({"messge","북마크 취소가 완료되었습니다!"},status=status.HTTP_200_OK)
