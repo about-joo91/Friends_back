6d455dcabe3904b41d7261222e064eb20a08421d
about-joo91 <about.joo91@gmail.com>
2022-07-06 15:14:46 +0900

    refac/url주소 변경

--- Friends_Back/joo_test/urls.py
+++ Friends_Back/joo_test/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.PostView.as_view()),
     path('<int:post_id>', views.PostView.as_view()),
-    path('priview/',views.PreviewView.as_view()),
+    path('preview/',views.PreviewView.as_view()),
     
 ]
